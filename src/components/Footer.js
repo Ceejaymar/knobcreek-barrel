a5a21ad138baff89f4e5d018e57edc4fe87faf8d
Carlos Martinez <martinezcj2@gmail.com>
2018-08-31 03:07:02 -0400

    Add external social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer__social">
         <span className="share-on">share on</span>
-        <a href="#" className="footer__social-link"><img src={facebook} alt="facebook"/></a>
-        <a href="#" className="footer__social-link"><img src={twitter} alt="twitter"/></a>
+        <a href="https://www.facebook.com/KnobCreek/" target="_blank" rel="noopener noreferrer" className="footer__social-link"><img src={facebook} alt="facebook"/></a>
+        <a href="https://twitter.com/knobcreek" target="_blank" rel="noopener noreferrer" className="footer__social-link"><img src={twitter} alt="twitter"/></a>
       </div>
         <img className="navbar__brand" src={brand} alt="brand name"/>
     </footer>
